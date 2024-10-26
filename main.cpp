50461cc4fa7573734df26c870658735961607505
Ali Saeed <asaeed5637@gmail.com>
2024-10-25 19:22:57 -0700

    #1 commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
+#include <set>
+#include <list>
+#include <string>
+#include <vector>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
+    ifstream fin("codes.txt");
     
+    auto start_read_v = high_resolution_clock::now();
+    vector<string> read_vect;
+    string random;
+    while(getline(fin, random)) {
+        read_vect.push_back(random);
+    }
+    auto end_read_v = high_resolution_clock::now();
+    auto duration_read_v = duration_cast<milliseconds>(end_read_v - start_read_v);
+    auto read_v = duration_read_v.count();
 
     return 0;
 }
