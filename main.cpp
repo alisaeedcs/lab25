562eba8d3d677d9eedac61b68ee65e04e9d52a67
Ali Saeed <asaeed5637@gmail.com>
2024-10-25 19:42:28 -0700

    #3 commit

--- main.cpp
+++ main.cpp
@@ -26,16 +26,28 @@ int main() {
     auto start_sort_v = high_resolution_clock::now();
     sort(read_vect.begin(), read_vect.end());
     auto end_sort_v = high_resolution_clock::now();
-    auto duration_sort_v = duration_cast<milliseconds>(end_read_v - start_read_v);
+    auto duration_sort_v = duration_cast<milliseconds>(end_sort_v - start_sort_v);
     auto sort_v = duration_sort_v.count();
 
     //vector: duration of insert
     auto start_insert_v = high_resolution_clock::now();
     read_vect.insert(read_vect.begin() + read_vect.size() / 2, "TESTCODE");
     auto end_insert_v = high_resolution_clock::now();
-    auto duration_insert_v = duration_cast<milliseconds>(end_read_v - start_read_v);
-    auto sort_v = duration_insert_v.count();
+    auto duration_insert_v = duration_cast<milliseconds>(end_insert_v - start_insert_v);
+    auto insert_v = duration_insert_v.count();
 
+    //vector: duration of delete
+    auto start_delete_v = high_resolution_clock::now();
+    read_vect.erase(read_vect.begin() + read_vect.size() / 2);
+    auto end_delete_v = high_resolution_clock::now();
+    auto duration_delete_v = duration_cast<milliseconds>(end_delete_v - start_delete_v);
+    auto delete_v = duration_delete_v.count();
+
+    cout << left << setw(10) << "Operation" << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
+    cout << left << setw(10) << "Read" << setw(10) << read_v;
+    cout << left << setw(10) << "Sort" << setw(10) << sort_v;
+    cout << left << setw(10) << "Insert" << setw(10) << insert_v;
+    cout << left << setw(10) << "Delete" << setw(10) << delete_v;
     return 0;
 }
 
