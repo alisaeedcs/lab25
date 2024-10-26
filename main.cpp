9968bf966696a72afe8ed47b06ad4474b19356de
Ali Saeed <asaeed5637@gmail.com>
2024-10-25 19:32:37 -0700

    #2 commit

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std::chrono;
 int main() {
     ifstream fin("codes.txt");
     
+    //vector: duration of read
     auto start_read_v = high_resolution_clock::now();
     vector<string> read_vect;
     string random;
@@ -20,6 +21,20 @@ int main() {
     auto end_read_v = high_resolution_clock::now();
     auto duration_read_v = duration_cast<milliseconds>(end_read_v - start_read_v);
     auto read_v = duration_read_v.count();
+
+    //vector: duration of sort
+    auto start_sort_v = high_resolution_clock::now();
+    sort(read_vect.begin(), read_vect.end());
+    auto end_sort_v = high_resolution_clock::now();
+    auto duration_sort_v = duration_cast<milliseconds>(end_read_v - start_read_v);
+    auto sort_v = duration_sort_v.count();
+
+    //vector: duration of insert
+    auto start_insert_v = high_resolution_clock::now();
+    read_vect.insert(read_vect.begin() + read_vect.size() / 2, "TESTCODE");
+    auto end_insert_v = high_resolution_clock::now();
+    auto duration_insert_v = duration_cast<milliseconds>(end_read_v - start_read_v);
+    auto sort_v = duration_insert_v.count();
 
     return 0;
 }
